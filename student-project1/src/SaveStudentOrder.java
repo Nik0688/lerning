562e926c241f8fb64eb5267858c56c189b4a8fe8
nik <nik@niknote>
2019-04-14 10:03:38 +0500

    lesson2

--- student-project1/src/SaveStudentOrder.java
+++ student-project1/src/SaveStudentOrder.java
@@ -2,7 +2,11 @@
 public class SaveStudentOrder {
 
 	public static void main(String[] args) {
-		System.out.println("Saving student in progress...");
+		saveStudentOrder();
+	}
+	
+	static void saveStudentOrder() {
+		System.out.println("saveStudentOrder");
 	}
 
 }
